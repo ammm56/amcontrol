24106f84a9d7b38975cfdc4d93808794c95c9580
am tang <tang.am@foxmail.com>
2026-03-09 09:24:49 +0800

    添加固高dll

---
+++ AMControlWPF/GSNA.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AMControlWPF
+{
+    /// <summary>
+    /// GSNA.xaml 的交互逻辑
+    /// </summary>
+    public partial class GSNA : Window
+    {
+        public GSNA()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_init_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_clearstatus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_enable_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_pointclearzero_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_runstaart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_runstop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
