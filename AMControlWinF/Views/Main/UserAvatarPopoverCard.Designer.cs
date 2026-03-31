8cf967d73ddab0c5be88322e3531bb470fe0049f
am tang <tang.am@foxmail.com>
2026-03-31 16:18:25 +0800

    用户框样式修改

--- AMControlWinF/Views/Main/UserAvatarPopoverCard.Designer.cs
+++ AMControlWinF/Views/Main/UserAvatarPopoverCard.Designer.cs
@@ -29,161 +29,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panelRoot = new AntdUI.Panel();
-            this.panelActions = new System.Windows.Forms.Panel();
-            this.buttonLogout = new AntdUI.Button();
-            this.buttonChangePassword = new AntdUI.Button();
-            this.buttonSwitchUser = new AntdUI.Button();
-            this.separatorHeader = new System.Windows.Forms.Panel();
-            this.panelHeader = new AntdUI.Panel();
-            this.labelRoleDisplayName = new AntdUI.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserAvatarPopoverCard));
+            this.avatarPopupUser = new AntdUI.Avatar();
             this.labelUserDisplayName = new AntdUI.Label();
-            this.panelRoot.SuspendLayout();
-            this.panelActions.SuspendLayout();
-            this.panelHeader.SuspendLayout();
+            this.labelRoleDisplayName = new AntdUI.Label();
+            this.buttonSwitchUser = new AntdUI.Button();
+            this.buttonChangePassword = new AntdUI.Button();
+            this.buttonLogout = new AntdUI.Button();
             this.SuspendLayout();
             // 
-            // panelRoot
+            // avatarPopupUser
             // 
-            this.panelRoot.BackColor = System.Drawing.Color.Transparent;
-            this.panelRoot.Controls.Add(this.panelActions);
-            this.panelRoot.Controls.Add(this.separatorHeader);
-            this.panelRoot.Controls.Add(this.panelHeader);
-            this.panelRoot.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelRoot.Location = new System.Drawing.Point(0, 0);
-            this.panelRoot.Margin = new System.Windows.Forms.Padding(0);
-            this.panelRoot.Name = "panelRoot";
-            this.panelRoot.Padding = new System.Windows.Forms.Padding(6);
-            this.panelRoot.Radius = 14;
-            this.panelRoot.Shadow = 8;
-            this.panelRoot.Size = new System.Drawing.Size(220, 220);
-            this.panelRoot.TabIndex = 0;
-            // 
-            // panelActions
-            // 
-            this.panelActions.BackColor = System.Drawing.Color.Transparent;
-            this.panelActions.Controls.Add(this.buttonLogout);
-            this.panelActions.Controls.Add(this.buttonChangePassword);
-            this.panelActions.Controls.Add(this.buttonSwitchUser);
-            this.panelActions.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelActions.Location = new System.Drawing.Point(18, 75);
-            this.panelActions.Margin = new System.Windows.Forms.Padding(0);
-            this.panelActions.Name = "panelActions";
-            this.panelActions.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
-            this.panelActions.Size = new System.Drawing.Size(184, 127);
-            this.panelActions.TabIndex = 2;
-            // 
-            // buttonLogout
-            // 
-            this.buttonLogout.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonLogout.Ghost = true;
-            this.buttonLogout.IconSvg = "LogoutOutlined";
-            this.buttonLogout.Location = new System.Drawing.Point(0, 90);
-            this.buttonLogout.Margin = new System.Windows.Forms.Padding(0);
-            this.buttonLogout.Name = "buttonLogout";
-            this.buttonLogout.Radius = 10;
-            this.buttonLogout.Size = new System.Drawing.Size(184, 40);
-            this.buttonLogout.TabIndex = 3;
-            this.buttonLogout.Text = "退出登录";
-            this.buttonLogout.WaveSize = 0;
-            // 
-            // buttonChangePassword
-            // 
-            this.buttonChangePassword.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonChangePassword.Ghost = true;
-            this.buttonChangePassword.IconSvg = "LockOutlined";
-            this.buttonChangePassword.Location = new System.Drawing.Point(0, 50);
-            this.buttonChangePassword.Margin = new System.Windows.Forms.Padding(0);
-            this.buttonChangePassword.Name = "buttonChangePassword";
-            this.buttonChangePassword.Radius = 10;
-            this.buttonChangePassword.Size = new System.Drawing.Size(184, 40);
-            this.buttonChangePassword.TabIndex = 2;
-            this.buttonChangePassword.Text = "修改密码";
-            this.buttonChangePassword.WaveSize = 0;
-            // 
-            // buttonSwitchUser
-            // 
-            this.buttonSwitchUser.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonSwitchUser.Ghost = true;
-            this.buttonSwitchUser.IconSvg = "SwapOutlined";
-            this.buttonSwitchUser.Location = new System.Drawing.Point(0, 10);
-            this.buttonSwitchUser.Margin = new System.Windows.Forms.Padding(0);
-            this.buttonSwitchUser.Name = "buttonSwitchUser";
-            this.buttonSwitchUser.Radius = 10;
-            this.buttonSwitchUser.Size = new System.Drawing.Size(184, 40);
-            this.buttonSwitchUser.TabIndex = 1;
-            this.buttonSwitchUser.Text = "切换用户";
-            this.buttonSwitchUser.WaveSize = 0;
-            // 
-            // separatorHeader
-            // 
-            this.separatorHeader.Dock = System.Windows.Forms.DockStyle.Top;
-            this.separatorHeader.Location = new System.Drawing.Point(18, 74);
-            this.separatorHeader.Margin = new System.Windows.Forms.Padding(0);
-            this.separatorHeader.Name = "separatorHeader";
-            this.separatorHeader.Size = new System.Drawing.Size(184, 1);
-            this.separatorHeader.TabIndex = 1;
-            this.separatorHeader.Visible = false;
-            // 
-            // panelHeader
-            // 
-            this.panelHeader.BackColor = System.Drawing.Color.Transparent;
-            this.panelHeader.Controls.Add(this.labelRoleDisplayName);
-            this.panelHeader.Controls.Add(this.labelUserDisplayName);
-            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelHeader.Location = new System.Drawing.Point(18, 18);
-            this.panelHeader.Margin = new System.Windows.Forms.Padding(0);
-            this.panelHeader.Name = "panelHeader";
-            this.panelHeader.Padding = new System.Windows.Forms.Padding(0, 4, 0, 6);
-            this.panelHeader.Radius = 0;
-            this.panelHeader.Size = new System.Drawing.Size(184, 56);
-            this.panelHeader.TabIndex = 0;
-            // 
-            // labelRoleDisplayName
-            // 
-            this.labelRoleDisplayName.Dock = System.Windows.Forms.DockStyle.Top;
-            this.labelRoleDisplayName.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
-            this.labelRoleDisplayName.Location = new System.Drawing.Point(0, 30);
-            this.labelRoleDisplayName.Margin = new System.Windows.Forms.Padding(0);
-            this.labelRoleDisplayName.Name = "labelRoleDisplayName";
-            this.labelRoleDisplayName.Size = new System.Drawing.Size(184, 22);
-            this.labelRoleDisplayName.TabIndex = 1;
-            this.labelRoleDisplayName.Text = "用户";
+            this.avatarPopupUser.ImageSvg = resources.GetString("avatarPopupUser.ImageSvg");
+            this.avatarPopupUser.Location = new System.Drawing.Point(16, 14);
+            this.avatarPopupUser.Margin = new System.Windows.Forms.Padding(0);
+            this.avatarPopupUser.Name = "avatarPopupUser";
+            this.avatarPopupUser.Padding = new System.Windows.Forms.Padding(4);
+            this.avatarPopupUser.Radius = 21;
+            this.avatarPopupUser.Size = new System.Drawing.Size(42, 42);
+            this.avatarPopupUser.TabIndex = 0;
+            this.avatarPopupUser.Text = "A";
             // 
             // labelUserDisplayName
             // 
-            this.labelUserDisplayName.Dock = System.Windows.Forms.DockStyle.Top;
-            this.labelUserDisplayName.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold);
-            this.labelUserDisplayName.Location = new System.Drawing.Point(0, 4);
+            this.labelUserDisplayName.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Bold);
+            this.labelUserDisplayName.Location = new System.Drawing.Point(68, 14);
             this.labelUserDisplayName.Margin = new System.Windows.Forms.Padding(0);
             this.labelUserDisplayName.Name = "labelUserDisplayName";
-            this.labelUserDisplayName.Size = new System.Drawing.Size(184, 26);
-            this.labelUserDisplayName.TabIndex = 0;
-            this.labelUserDisplayName.Text = "未登录";
+            this.labelUserDisplayName.Size = new System.Drawing.Size(180, 22);
+            this.labelUserDisplayName.TabIndex = 1;
+            this.labelUserDisplayName.Text = "当前用户";
+            // 
+            // labelRoleDisplayName
+            // 
+            this.labelRoleDisplayName.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.labelRoleDisplayName.Location = new System.Drawing.Point(68, 40);
+            this.labelRoleDisplayName.Margin = new System.Windows.Forms.Padding(0);
+            this.labelRoleDisplayName.Name = "labelRoleDisplayName";
+            this.labelRoleDisplayName.Size = new System.Drawing.Size(180, 18);
+            this.labelRoleDisplayName.TabIndex = 2;
+            this.labelRoleDisplayName.Text = "角色";
+            // 
+            // buttonSwitchUser
+            // 
+            this.buttonSwitchUser.Location = new System.Drawing.Point(16, 74);
+            this.buttonSwitchUser.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonSwitchUser.Name = "buttonSwitchUser";
+            this.buttonSwitchUser.Radius = 10;
+            this.buttonSwitchUser.Size = new System.Drawing.Size(110, 36);
+            this.buttonSwitchUser.TabIndex = 3;
+            this.buttonSwitchUser.Text = "切换用户";
+            this.buttonSwitchUser.Type = AntdUI.TTypeMini.Primary;
+            this.buttonSwitchUser.WaveSize = 0;
+            // 
+            // buttonChangePassword
+            // 
+            this.buttonChangePassword.Location = new System.Drawing.Point(134, 74);
+            this.buttonChangePassword.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonChangePassword.Name = "buttonChangePassword";
+            this.buttonChangePassword.Radius = 10;
+            this.buttonChangePassword.Size = new System.Drawing.Size(110, 36);
+            this.buttonChangePassword.TabIndex = 4;
+            this.buttonChangePassword.Text = "修改密码";
+            this.buttonChangePassword.Type = AntdUI.TTypeMini.Primary;
+            this.buttonChangePassword.WaveSize = 0;
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Location = new System.Drawing.Point(16, 110);
+            this.buttonLogout.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Radius = 10;
+            this.buttonLogout.Size = new System.Drawing.Size(110, 36);
+            this.buttonLogout.TabIndex = 5;
+            this.buttonLogout.Text = "退出登录";
+            this.buttonLogout.Type = AntdUI.TTypeMini.Error;
+            this.buttonLogout.WaveSize = 0;
             // 
             // UserAvatarPopoverCard
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackColor = System.Drawing.Color.Transparent;
-            this.Controls.Add(this.panelRoot);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.buttonChangePassword);
+            this.Controls.Add(this.buttonSwitchUser);
+            this.Controls.Add(this.labelRoleDisplayName);
+            this.Controls.Add(this.labelUserDisplayName);
+            this.Controls.Add(this.avatarPopupUser);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "UserAvatarPopoverCard";
-            this.Size = new System.Drawing.Size(220, 220);
-            this.panelRoot.ResumeLayout(false);
-            this.panelActions.ResumeLayout(false);
-            this.panelHeader.ResumeLayout(false);
+            this.Size = new System.Drawing.Size(250, 160);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private AntdUI.Panel panelRoot;
-        private AntdUI.Panel panelHeader;
+        private AntdUI.Avatar avatarPopupUser;
         private AntdUI.Label labelUserDisplayName;
         private AntdUI.Label labelRoleDisplayName;
-        private System.Windows.Forms.Panel separatorHeader;
-        private System.Windows.Forms.Panel panelActions;
         private AntdUI.Button buttonSwitchUser;
         private AntdUI.Button buttonChangePassword;
         private AntdUI.Button buttonLogout;
--- AMControlWinF/Views/Main/UserAvatarPopoverCard.cs
+++ AMControlWinF/Views/Main/UserAvatarPopoverCard.cs
@@ -6,13 +6,18 @@ namespace AMControlWinF.Views.Main
 {
     /// <summary>
     /// 用户头像弹出操作卡片。
-    /// 使用 AntdUI 默认原生样式，自动跟随主题切换。
+    /// 使用最简单的 WinForms 布局，按钮使用 AntdUI。
     /// </summary>
     public partial class UserAvatarPopoverCard : UserControl
     {
         private string _userDisplayName;
         private string _roleDisplayName;
         private string _language;
+
+        private int _topActionButtonHeight;
+        private int _bottomActionButtonHeight;
+        private int _sectionGapHeight;
+        private int _actionColumnGap;
 
         public event EventHandler SwitchUserRequested;
         public event EventHandler ChangePasswordRequested;
@@ -31,10 +36,14 @@ namespace AMControlWinF.Views.Main
             _roleDisplayName = "用户";
             _language = "zh-CN";
 
+            _topActionButtonHeight = 36;
+            _bottomActionButtonHeight = 36;
+            _sectionGapHeight = 18;
+            _actionColumnGap = 8;
+
             BindEvents();
             RefreshDisplay();
-            ApplyLanguage(_language);
-            ApplyTheme(false);
+            LayoutControlsCore();
         }
 
         public string UserDisplayName
@@ -57,6 +66,46 @@ namespace AMControlWinF.Views.Main
             }
         }
 
+        public int TopActionButtonHeight
+        {
+            get { return _topActionButtonHeight; }
+            set
+            {
+                _topActionButtonHeight = value < 28 ? 28 : value;
+                LayoutControlsCore();
+            }
+        }
+
+        public int BottomActionButtonHeight
+        {
+            get { return _bottomActionButtonHeight; }
+            set
+            {
+                _bottomActionButtonHeight = value < 28 ? 28 : value;
+                LayoutControlsCore();
+            }
+        }
+
+        public int SectionGapHeight
+        {
+            get { return _sectionGapHeight; }
+            set
+            {
+                _sectionGapHeight = value < 0 ? 0 : value;
+                LayoutControlsCore();
+            }
+        }
+
+        public int ActionColumnGap
+        {
+            get { return _actionColumnGap; }
+            set
+            {
+                _actionColumnGap = value < 0 ? 0 : value;
+                LayoutControlsCore();
+            }
+        }
+
         public void SetUserInfo(string userDisplayName, string roleDisplayName)
         {
             _userDisplayName = string.IsNullOrWhiteSpace(userDisplayName) ? GetDefaultUserDisplayName() : userDisplayName.Trim();
@@ -72,8 +121,7 @@ namespace AMControlWinF.Views.Main
 
         public void ApplyTheme(bool isDarkMode)
         {
-            // 不做任何手动颜色覆盖。
-            // 由 AntdUI 原生主题系统自动接管颜色与样式。
+            // 不手动指定颜色，交由 AntdUI 原生主题处理。
         }
 
         private void BindEvents()
@@ -81,6 +129,104 @@ namespace AMControlWinF.Views.Main
             buttonSwitchUser.Click += ButtonSwitchUser_Click;
             buttonChangePassword.Click += ButtonChangePassword_Click;
             buttonLogout.Click += ButtonLogout_Click;
+            Resize += UserAvatarPopoverCard_Resize;
+        }
+
+        private void UserAvatarPopoverCard_Resize(object sender, EventArgs e)
+        {
+            LayoutControlsCore();
+        }
+
+        private void LayoutControlsCore()
+        {
+            SuspendLayout();
+
+            try
+            {
+                const int paddingLeft = 16;
+                const int paddingTop = 14;
+                const int paddingRight = 16;
+                const int paddingBottom = 16;
+                const int avatarSize = 42;
+                const int avatarTextGap = 10;
+                const int userNameHeight = 22;
+                const int roleHeight = 18;
+
+                var contentWidth = Width - paddingLeft - paddingRight;
+                if (contentWidth < 180)
+                {
+                    contentWidth = 180;
+                }
+
+                var textX = paddingLeft + avatarSize + avatarTextGap;
+                var textWidth = contentWidth - avatarSize - avatarTextGap;
+                if (textWidth < 80)
+                {
+                    textWidth = 80;
+                }
+
+                avatarPopupUser.SetBounds(
+                    paddingLeft,
+                    paddingTop,
+                    avatarSize,
+                    avatarSize);
+
+                labelUserDisplayName.SetBounds(
+                    textX,
+                    paddingTop,
+                    textWidth,
+                    userNameHeight);
+
+                labelRoleDisplayName.SetBounds(
+                    textX,
+                    paddingTop + userNameHeight + 4,
+                    textWidth,
+                    roleHeight);
+
+                var buttonTopY = paddingTop + avatarSize + _sectionGapHeight;
+                var leftButtonWidth = (contentWidth - _actionColumnGap) / 2;
+                if (leftButtonWidth < 80)
+                {
+                    leftButtonWidth = 80;
+                }
+
+                var rightButtonX = paddingLeft + leftButtonWidth + _actionColumnGap;
+                var rightButtonWidth = contentWidth - leftButtonWidth - _actionColumnGap;
+                if (rightButtonWidth < 80)
+                {
+                    rightButtonWidth = 80;
+                }
+
+                buttonSwitchUser.SetBounds(
+                    paddingLeft,
+                    buttonTopY,
+                    leftButtonWidth,
+                    _topActionButtonHeight);
+
+                buttonChangePassword.SetBounds(
+                    rightButtonX,
+                    buttonTopY,
+                    rightButtonWidth,
+                    _topActionButtonHeight);
+
+                buttonLogout.SetBounds(
+                    paddingLeft,
+                    buttonTopY + _topActionButtonHeight + 8,
+                    leftButtonWidth,
+                    _bottomActionButtonHeight);
+
+                var totalHeight = buttonLogout.Bottom + paddingBottom;
+                if (Height != totalHeight)
+                {
+                    Height = totalHeight;
+                }
+
+                MinimumSize = new System.Drawing.Size(240, totalHeight);
+            }
+            finally
+            {
+                ResumeLayout();
+            }
         }
 
         private void ButtonSwitchUser_Click(object sender, EventArgs e)
@@ -126,9 +272,32 @@ namespace AMControlWinF.Views.Main
                 ? GetDefaultRoleDisplayName()
                 : _roleDisplayName;
 
-            buttonSwitchUser.Text = IsEnglishLanguage(_language) ? "Switch User" : "切换用户";
-            buttonChangePassword.Text = IsEnglishLanguage(_language) ? "Change Password" : "修改密码";
-            buttonLogout.Text = IsEnglishLanguage(_language) ? "Logout" : "退出登录";
+            if (IsEnglishLanguage(_language))
+            {
+                buttonSwitchUser.Text = "Switch User";
+                buttonChangePassword.Text = "Change Password";
+                buttonLogout.Text = "Logout";
+            }
+            else
+            {
+                buttonSwitchUser.Text = "切换用户";
+                buttonChangePassword.Text = "修改密码";
+                buttonLogout.Text = "退出登录";
+            }
+
+            RefreshAvatarText();
+        }
+
+        private void RefreshAvatarText()
+        {
+            if (!string.IsNullOrWhiteSpace(avatarPopupUser.ImageSvg))
+            {
+                avatarPopupUser.Text = string.Empty;
+                return;
+            }
+
+            var text = string.IsNullOrWhiteSpace(_userDisplayName) ? "A" : _userDisplayName.Trim();
+            avatarPopupUser.Text = text.Substring(0, 1).ToUpperInvariant();
         }
 
         private string GetDefaultUserDisplayName()
